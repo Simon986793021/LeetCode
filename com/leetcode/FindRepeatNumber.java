95b51c6be51efec7a55bc545f5edcbe7ee5d7245
anjoyzhang <anjoyzhang@zego.im>
2020-11-04 09:42:49 +0800

    first commit

---
+++ com/leetcode/FindRepeatNumber.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+public class FindRepeatNumber{
+    public static void main (String argss[]){
+        int test []=new int [] {2,3,1,1,4,2,6};
+        System.out.println("数组中第一个重复的数字为："+findRepeatNumber(test));
+    }
+    private static int findRepeatNumber(int nums[]){
+        int temp[] = new int[nums.length];
+        for(int num :nums){
+            temp[num]++;
+            if(temp[num]>1){
+                return num;
+            }
+        }
+        return -1;
+    }
+}
